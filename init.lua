ed7dfacf5860393e6d558c168548f4b3b1dad1d8
Daulet Aibazov <dwlt_ybz@proton.me>
2025-06-29 17:22:39 -0500

    better code completion plugin; better escape; better fuzzy search

--- init.lua
+++ init.lua
@@ -38,3 +38,11 @@ require("nvim-tree").setup({
 vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format()
 end, { desc = "Format with LSP" })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Escape insert mode", noremap = true })
